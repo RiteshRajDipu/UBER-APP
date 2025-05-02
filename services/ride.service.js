071e24fb6dd8786f2ede05ca60dfeadef4c71625
Ritesh Raj <riteshrajr123rs@gmail.com>
2025-05-02 18:27:53 +0530

    services created

---
+++ services/ride.service.js
@@ -0,0 +1,27 @@
+
+
+async function getFare() {
+
+    const baseFare = {
+        auto: 30,
+        car: 50,
+        moto: 20
+    }
+
+    const perKmRate = {
+        auto: 10,
+        car: 15,
+        moto: 8
+    }
+
+    perMinuteRate = {
+        auto: 2,
+        car: 3,
+        moto: 1.5
+    }
+ 
+ const fare = {
+    
+ }
+
+} 
