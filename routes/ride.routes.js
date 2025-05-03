951c12e6a055bb315c56415c0b9142bb909ffa5b
Ritesh Raj <riteshrajr123rs@gmail.com>
2025-05-03 13:07:48 +0530

    postman are working fine till otp

--- routes/ride.routes.js
+++ routes/ride.routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const authMiddleware = require('../middlewares/auth.middleware');
+const rideController = require('../controllers/ride.controller');
 
 router.post('/create', 
     authMiddleware.authUser,
     body('pickup').isString().isLength({min: 3}).withMessage('Invalid Pickup address'),
     body('destination').isString().isLength({min: 3}).withMessage('Invalid destination address'),
-    body('vehicleType').isString().isIn([ 'auto', 'car', 'moto' ]).withMessage('Invalid vehicle type')
+    body('vehicleType').isString().isIn([ 'auto', 'car', 'moto' ]).withMessage('Invalid vehicle type'),
+    rideController.createRide
 )
 
-export default router;
+module.exports = router;
